ccbabf2f4b049b26c66b65e1bd3ccee9d59e6f2a
iii-source <quercus1109@gmail.com>
2021-05-24 00:32:15 +0900

    update test_test01.py AP DB 各機能テスト実施

--- tests/test_test01.py
+++ tests/test_test01.py
@@ -1,5 +1,6 @@
-from psycopg2.extras import DictCursor, psycopg2
-import os
+import requests
+import data
+
 
 def test_1():
     a = 1
@@ -8,10 +9,15 @@ def test_1():
 
 
 def test_2():
-    print(os.environ.get("DATABASE_URL"))
-    connector = psycopg2.connect(os.environ.get("DATABASE_URL"))
+    result = requests.get("http://127.0.0.1:5000/")
+    print(result.text)
+    assert result.text == "Hello World!"
 
 
+def test_3():
+    result = data.db_test03()
+    for record in result['records']:
+        assert record['test_id'] == 1
 
 
 
