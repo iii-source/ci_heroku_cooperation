ed6185eb0935add5363de0c054210c96724ca30c
iii-source <quercus1109@gmail.com>
2021-05-23 23:52:24 +0900

    update DB test

--- tests/test_test01.py
+++ tests/test_test01.py
@@ -1,21 +1,17 @@
-import hello_world
-import unittest
+from psycopg2.extras import DictCursor, psycopg2
+import os
+
+def test_1():
+    a = 1
+    b = 1
+    assert a == b
 
 
-class TestHello(unittest.TestCase):
-
-    def setUp(self):
-        self.app = hello_world.app.test_client()
-        self.app.testing = True
-
-    def test_status_code(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_flask_simple(self):
-        result = self.app.get('/')
-        assert b'HelloWorld' == result.data
+def test_2():
+    print(os.environ.get("DATABASE_URL"))
+    connector = psycopg2.connect(os.environ.get("DATABASE_URL"))
 
 
-if __name__ == '__main__':
-    unittest.main()
+
+
+
