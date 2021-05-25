f19414f7d32094ae2a8e6cf0ff8820305a377f1d
iii-source <quercus1109@gmail.com>
2021-05-25 15:35:53 +0900

    update tests/test_test01.py DBテスト機能追加

--- tests/test_test01.py
+++ tests/test_test01.py
@@ -2,20 +2,30 @@ import requests
 import data
 
 
+# 基本的な動作確認
 def test_1():
     a = 1
     b = 1
     assert a == b
 
 
+# APIの動作確認
 def test_2():
     result = requests.get("http://127.0.0.1:5000/")
     print(result.text)
-    assert result.text == "Hello World!"
+    assert result.text == "Hello"
 
 
+# LocalDBの動作確認
 def test_3():
-    result = data.db_test03()
+    result = data.db_test()
+    for record in result['records']:
+        assert record['test_id'] == 1
+
+
+# API経由でDBの動作確認
+def test_4():
+    result = requests.get("http://127.0.0.1:5000/db-test").json()
     for record in result['records']:
         assert record['test_id'] == 1
 
