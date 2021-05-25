bb1149a1b99e00c8fe52a65be37e4806c9005234
iii-source <quercus1109@gmail.com>
2021-05-25 15:36:52 +0900

    update server/app.py db接続機能追加

--- server/app.py
+++ server/app.py
@@ -1,12 +1,23 @@
 from flask import Flask
+import os
+import data
+
+
 app = Flask(__name__)
 
 
 # http://127.0.0.1:5000/
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello"
+
+
+@app.route('/db-test')
+def db_test():
+    result = data.db_test()
+    return result
 
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
